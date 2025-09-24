527bdda8552f0789a41cea1c080559c95d3964d5
tyw <yt2895@outlook.com>
2025-09-24 16:18:20 -0400

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         //TIP 当文本光标位于高亮显示的文本处时按 <shortcut actionId="ShowIntentionActions"/>
         // 查看 IntelliJ IDEA 建议如何修正。
         System.out.println("Hello and welcome!");
-
+//ok why
         for (int i = 1; i <= 5; i++) {
             //TIP 按 <shortcut actionId="Debug"/> 开始调试代码。我们已经设置了一个 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 断点
             // 但您始终可以通过按 <shortcut actionId="ToggleLineBreakpoint"/> 添加更多断点。
